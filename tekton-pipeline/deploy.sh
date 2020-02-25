b07cf81e2018aec46d30eef4822d541dd0abf4d6
vincent-cailly <vincent.cailly@fr.ibm.com>
2020-02-25 11:46:31 +0100

    12th commit

--- tekton-pipeline/deploy.sh
+++ tekton-pipeline/deploy.sh
@@ -6,14 +6,14 @@ oc delete sts -n demo mqtest-ibm-mq
 
 oc delete pvc -n demo data-mqtest-ibm-mq-0 data-mqtest-ibm-mq-1 mqtest-ibm-mq-log mqtest-ibm-mq-qm
 #oc delete svc -n demo mqtest-ibm-mq mqtest-ibm-mq-metrics
-sleep 2
+sleep 5
 kubectl patch pvc mqtest-ibm-mq-log -n demo -p '{"metadata":{"finalizers": []}}' --type=merge
 kubectl patch pvc mqtest-ibm-mq-qm -n demo -p '{"metadata":{"finalizers": []}}' --type=merge
 kubectl patch pvc data-mqtest-ibm-mq-0 -n demo -p '{"metadata":{"finalizers": []}}' --type=merge
 kubectl patch pvc data-mqtest-ibm-mq-1 -n demo -p '{"metadata":{"finalizers": []}}' --type=merge
 
-
 ibmcloud cr image-rm de.icr.io/vcailly/builtmq:9.1.3
+sleep 5
 
 cd ..
 git add * -A
