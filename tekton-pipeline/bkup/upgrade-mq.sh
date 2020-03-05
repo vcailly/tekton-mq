6303350b140c638dabe2c6c530c6152f4d9e78d4
vincent-cailly <vincent.cailly@fr.ibm.com>
2020-03-05 15:03:36 +0100

    12th commit

--- tekton-pipeline/bkup/upgrade-mq.sh
+++ tekton-pipeline/bkup/upgrade-mq.sh
@@ -76,7 +76,7 @@ echo "Backup Data: done"
 echo ""
 echo "Patch statefullset with new MQ image version "
 echo "---------------------------------------------"
-kubectl patch sts  ${sts} -n ${nspace} --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value":"ibmcom/mq:9.1.4.0-r1"}]'
+kubectl patch sts  ${sts} -n ${nspace} --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value":"vcailly/builtmq:9.1.4.0"}]'
 
 echo ''
 echo "Patch statefullset with new MQ image version : done"
