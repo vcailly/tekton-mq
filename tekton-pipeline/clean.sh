b08d011548f525bddef5a5fbee8f48a749633c2a
vincent-cailly <vincent.cailly@fr.ibm.com>
2020-03-18 23:14:52 +0100

    12th commit

--- tekton-pipeline/clean.sh
+++ tekton-pipeline/clean.sh
@@ -12,18 +12,4 @@ kubectl patch pvc mqtest-ibm-mq-qm -n demo -p '{"metadata":{"finalizers": []}}' 
 kubectl patch pvc data-mqtest-ibm-mq-0 -n demo -p '{"metadata":{"finalizers": []}}' --type=merge
 kubectl patch pvc data-mqtest-ibm-mq-1 -n demo -p '{"metadata":{"finalizers": []}}' --type=merge
 
-#ibmcloud cr image-rm de.icr.io/vcailly/bsmmq:9.1.3.0-r3
-sleep 5
-
-cd ..
-git add * -A
-git commit -m "12th commit"
-git push
-cd tekton-pipeline
-oc apply -f resources/git.yaml
-oc apply -f pipeline/pipeline.yaml
-oc apply -f task/build-src-code.yaml 
-oc apply -f task/deploy-to-cluster.yaml 
-oc delete pr mq-pipeline-run
-oc apply -f pipeline/pipeline-run.yaml
 
